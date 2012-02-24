b5d63690ff4dac1094099c93e2362cd51f6bd848
5709775@gmail.com <5709775@gmail.com@4af9759c-1e4e-65e3-01cf-fac85bfff9da>
2012-02-24 06:00:59 +0000

    更新版本信息

--- trunk/app/base/Constant.class.php
+++ trunk/app/base/Constant.class.php
@@ -12,6 +12,6 @@ class base_Constant {
 	const COOKIE_KEY = "1b04c22c8bbf0ad9cda884d86ceb653b"; //cookie密匙
 	const TEMP_DIR = "simpla"; //模版目录
 	const DEFAULT_TITLE = "超市进销存管理系统SmPSSv1.0"; //默认网站标题
-	const VERSION = "v1.0 beta";
+	const VERSION = "v1.0 Release";
 	const BARCODE = "69099999";//条形码1-8位 对于没有条形码的商品我们默认分配的代码
 }
